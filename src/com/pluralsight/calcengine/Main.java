ca658fdf729049a337ab78e63ff9b728357d1392
Aldair <aldair_fime@hotmail.com>
2024-01-16 00:07:17 -0600

    class: Understanding Java Classes and Objects

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -6,46 +6,50 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = { 'd', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
-
-        if(args.length == 0) {
-            for (int i = 0; i < opCodes.length; i++) {
-                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-            }
-
-            for (double currentResult : results) {
-                System.out.println(currentResult);
-            }
-        } else if (args.length == 1 && args[0].equals("interactive")) {
-            executeInteractively();
-        } else if(args.length == 3) {
-            handleCommandLine(args);
-        } else
-            System.out.println("Please provide an operation code and 2 numeric values");
+        performCalculations();
     }
+    static void performCalculations() {
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0d, 'm');
 
-    static void executeInteractively() {
-        System.out.println("Enter an operation and two numbers");
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        String[] parts = userInput.split(" ");
-        performOperation(parts);
-    }
-
-    private static void performOperation(String[] parts) {
-        char opCode = opCodeFromString(parts[0]);
-        if(opCode == 'w')
-            handleWhen(parts);
-        else {
-            double leftVal = valueFromWord(parts[1]);
-            double rightVal = valueFromWord(parts[2]);
-            double result = execute(opCode, leftVal, rightVal);
-            displayResult(opCode, leftVal, rightVal, result);
+        for(MathEquation equation: equations) {
+            equation.execute();
+            System.out.println("result: " + equation.result);
         }
+
     }
+
+    private static MathEquation create(double leftVal, double rightVal, char opCode) {
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+        return equation;
+    }
+
+
+//    static void executeInteractively() {
+//        System.out.println("Enter an operation and two numbers");
+//        Scanner scanner = new Scanner(System.in);
+//        String userInput = scanner.nextLine();
+//        String[] parts = userInput.split(" ");
+//        performOperation(parts);
+//    }
+
+//    private static void performOperation(String[] parts) {
+//        char opCode = opCodeFromString(parts[0]);
+//        if(opCode == 'w')
+//            handleWhen(parts);
+//        else {
+//            double leftVal = valueFromWord(parts[1]);
+//            double rightVal = valueFromWord(parts[2]);
+//            double result = execute(opCode, leftVal, rightVal);
+//            displayResult(opCode, leftVal, rightVal, result);
+//        }
+//    }
 
     private static void handleWhen(String[] parts) {
         LocalDate startDate = LocalDate.parse(parts[1]);
@@ -84,36 +88,13 @@ public class Main {
         return symbol;
     }
 
-    private static void handleCommandLine(String[] args) {
-        char opCode = args[0].charAt(0);
-        double leftVal = Double.parseDouble(args[1]);
-        double rightVal = Double.parseDouble(args[2]);
-        double result = execute(opCode, leftVal, rightVal);
-        System.out.println(result);
-    }
-
-    static double execute(char opCode, double leftVal, double rightVal) {
-        double result;
-        switch (opCode) {
-            case 'a':
-                result = leftVal + rightVal;
-                break;
-            case 's':
-                result = leftVal - rightVal;
-                break;
-            case 'm':
-                result = leftVal * rightVal;
-                break;
-            case 'd':
-                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
-                break;
-            default:
-                System.out.println(("Invalid opCode: " + opCode));
-                result = 0.0d;
-                break;
-        }
-        return result;
-    }
+//    private static void handleCommandLine(String[] args) {
+//        char opCode = args[0].charAt(0);
+//        double leftVal = Double.parseDouble(args[1]);
+//        double rightVal = Double.parseDouble(args[2]);
+//        double result = execute(opCode, leftVal, rightVal);
+//        System.out.println(result);
+//    }
 
     static char opCodeFromString(String operationName) {
         return operationName.charAt(0);
